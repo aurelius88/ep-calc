507cb0a4bd201f00c9dd0de5bd51d3c08957cf91
Aurelius <aurelius.dev@gmail.com>
2019-02-19 01:33:00 +0100

    Activate EP exp calculations feature, modify README and move data files
    - Add ep exp calculation for left exp before soft cap is exceeded
    Features now available:
    - exp by ep and percent
    - level by ep
    - left exp by ep and percent
    
    Update README to current commands and add new todos.
    
    Add data files:
    - ep-sources (moved from index.js)
    - soft-cap (map: ep -> soft-cap)
    - ep (accumulated ep array, index corresponds to the level)

---
+++ data/ep-sources.js
@@ -0,0 +1,47 @@
+module.exports = new Map([
+    [
+        "batuDesert",
+        { exp: 2000.0, expAfterLimit: 1600.0, isQuest: true, limit: 16, asFiller: false, time: 1200, bams: 0 }
+    ],
+    [
+        "dungeon439",
+        { exp: 1518.65, expAfterLimit: 1215.0, isQuest: true, limit: 16, asFiller: true, time: 720, bams: 0 }
+    ],
+    [
+        "CorsairsFraywindSkyring",
+        { exp: 1518.65, expAfterLimit: 1215.0, isQuest: true, limit: 16, asFiller: false, time: 1200, bams: 0 }
+    ],
+    [
+        "dungeon431",
+        { exp: 1418.0, expAfterLimit: 1134.0, isQuest: true, limit: 16, asFiller: true, time: 360, bams: 3 }
+    ],
+    [
+        "dungeon412",
+        { exp: 1267.7, expAfterLimit: 1012.0, isQuest: true, limit: 16, asFiller: true, time: 180, bams: 3 }
+    ],
+    [
+        "levelUp65_2",
+        { exp: 1000.0, expAfterLimit: 1000.0, isQuest: false, limit: 1, asFiller: false, time: -1, bams: 0 }
+    ],
+    [
+        "islandOfDawn",
+        { exp: 911.6, expAfterLimit: 729.0, isQuest: true, limit: 16, asFiller: true, time: 80, bams: 10 }
+    ],
+    ["echoesOfAranea", { exp: 911.0, expAfterLimit: 0, isQuest: true, limit: 16, asFiller: true, time: 120, bams: 0 }],
+    ["kumasIronBG", { exp: 843.0, expAfterLimit: 0, isQuest: true, limit: 16, asFiller: false, time: 1200, bams: 0 }],
+    ["fishing", { exp: 646.67, expAfterLimit: 517.3, isQuest: true, limit: 1, asFiller: true, time: 120, bams: 0 }],
+    [
+        "guardianAndFlyingVanguard",
+        { exp: 500.0, expAfterLimit: 400.0, isQuest: true, limit: 16, asFiller: true, time: 240, bams: 0 }
+    ],
+    ["pitOfPetrax", { exp: 454.65, expAfterLimit: 363, isQuest: true, limit: 3, asFiller: true, time: 60, bams: 1 }],
+    [
+        "celestialArena",
+        { exp: 425.0, expAfterLimit: 340, isQuest: true, limit: 16, asFiller: true, time: 300, bams: 0 }
+    ],
+    ["aceDungeons", { exp: 303.0, expAfterLimit: 242.4, isQuest: true, limit: 16, asFiller: true, time: 150, bams: 4 }],
+    ["kill30Quest", { exp: 270.0, expAfterLimit: 216, isQuest: true, limit: 16, asFiller: true, time: 300, bams: 0 }],
+    ["gather30Quest", { exp: 180.0, expAfterLimit: 144, isQuest: true, limit: 16, asFiller: true, time: 180, bams: 0 }],
+    ["bam", { exp: 10.0, expAfterLimit: 10.0, isQuest: false, limit: -1, asFiller: true, time: 8, bams: 0 }],
+    ["levelUp65_1", { exp: 1.0, expAfterLimit: 1.0, isQuest: false, limit: 1, asFiller: false, time: -1, bams: 0 }]
+]);

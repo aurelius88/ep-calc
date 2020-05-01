cf35fd8947a58d06ffc274a067ad5691a8bdbd05
Aurelius <aurelius.dev@gmail.com>
2020-05-01 12:59:21 +0200

    Add locale for dungeon names. Change 439+ to 435+ dungeons and adept related localisations. Change version and repository server url in module to fit dev branch.

--- data/ep-sources.js
+++ data/ep-sources.js
@@ -1,30 +1,32 @@
 module.exports = new Map([
-    ["batuDesert", { exp: 2000, expAfterLimit: 1600, isQuest: true, limit: 16, asFiller: false, time: 1200, bams: 0 }],
-    ["dungeon439", { exp: 1518, expAfterLimit: 1214, isQuest: true, limit: 16, asFiller: true, time: 720, bams: 0 }],
+    ["batuDesert", { exp: 2000, expAfterLimit: 1600, isQuest: true, limit: 1, asFiller: false, time: 1200, bams: 0 }],
+    ["dungeon435", { exp: 1518, expAfterLimit: 1215, isQuest: true, limit: Infinity, asFiller: true, time: 720, bams: 0 }],
     [
         "CorsairsFraywindSkyring",
-        { exp: 1518, expAfterLimit: 1214, isQuest: true, limit: 16, asFiller: false, time: 1200, bams: 0 }
+        { exp: 1518, expAfterLimit: 1214, isQuest: true, limit: 3, asFiller: false, time: 1200, bams: 0 }
     ],
-    ["dungeon431", { exp: 1418, expAfterLimit: 1134, isQuest: true, limit: 16, asFiller: true, time: 360, bams: 3 }],
-    ["dungeon412", { exp: 1267, expAfterLimit: 1012, isQuest: true, limit: 16, asFiller: true, time: 180, bams: 3 }],
+    ["dungeon431", { exp: 1418, expAfterLimit: 1134, isQuest: true, limit: Infinity, asFiller: true, time: 360, bams: 3 }],
+    ["dungeon412", { exp: 1267, expAfterLimit: 1012, isQuest: true, limit: Infinity, asFiller: true, time: 180, bams: 3 }],
     [
         "wintera", // Wintera
-        { exp: 1215, expAfterLimit: 972, isQuest: true, limit: 1, asFiller: true, time: 300, bams: 0 }
+        { exp: 1215, expAfterLimit: 972, isQuest: true, limit: 1, asFiller: false, time: 300, bams: 0 }
     ],
     ["levelUp65_2", { exp: 1000, expAfterLimit: 1000, isQuest: false, limit: 1, asFiller: false, time: Infinity, bams: 0 }],
-    ["islandOfDawn", { exp: 911, expAfterLimit: 729, isQuest: true, limit: 16, asFiller: true, time: 80, bams: 10 }],
-    ["echoesOfAranea", { exp: 911, expAfterLimit: 729, isQuest: true, limit: 16, asFiller: true, time: 120, bams: 0 }],
-    ["kumasIronBG", { exp: 843, expAfterLimit: 674, isQuest: true, limit: 16, asFiller: true, time: 1000, bams: 0 }],
-    ["fishing", { exp: 646, expAfterLimit: 517, isQuest: true, limit: 1, asFiller: true, time: 120, bams: 0 }],
+    ["islandOfDawn", { exp: 911, expAfterLimit: 729, isQuest: true, limit: Infinity, asFiller: true, time: 110, bams: 10 }],
+    ["echoesOfAranea", { exp: 911, expAfterLimit: 729, isQuest: true, limit: 2, asFiller: true, time: 180, bams: 3 }],
+    ["kumas", { exp: 843, expAfterLimit: 674, isQuest: true, limit: Infinity, asFiller: true, time: 180, bams: 0 }],
+    ["ironBG", { exp: 843, expAfterLimit: 674, isQuest: true, limit: Infinity, asFiller: true, time: 1000, bams: 0 }],
+    ["fishing", { exp: 646, expAfterLimit: 517, isQuest: true, limit: 1, asFiller: false, time: 120, bams: 0 }],
     [
         "guardianAndFlyingVanguard",
-        { exp: 500, expAfterLimit: 400, isQuest: true, limit: 16, asFiller: true, time: 240, bams: 0 }
+        { exp: 500, expAfterLimit: 400, isQuest: true, limit: 2, asFiller: true, time: 240, bams: 0 }
     ],
-    ["pitOfPetrax", { exp: 454, expAfterLimit: 363, isQuest: true, limit: 3, asFiller: true, time: 60, bams: 1 }],
-    ["celestialArena", { exp: 425, expAfterLimit: 340, isQuest: true, limit: 16, asFiller: true, time: 300, bams: 0 }],
-    ["aceDungeons", { exp: 303, expAfterLimit: 242, isQuest: true, limit: 16, asFiller: true, time: 150, bams: 4 }],
-    ["kill30Quest", { exp: 270, expAfterLimit: 216, isQuest: true, limit: 16, asFiller: true, time: 300, bams: 0 }],
-    ["gather30Quest", { exp: 180, expAfterLimit: 144, isQuest: true, limit: 16, asFiller: true, time: 180, bams: 0 }],
+    ["pitOfPetrax", { exp: 455, expAfterLimit: 243, isQuest: true, limit: 3, asFiller: true, time: 60, bams: 1 }],
+    ["celestialArena", { exp: 425, expAfterLimit: 340, isQuest: true, limit: Infinity, asFiller: true, time: 200, bams: 0 }],
+    ["aceDungeonLili", { exp: 425, expAfterLimit: 324, isQuest: true, limit: 2, asFiller: true, time: 30, bams: 1 }],
+    ["aceDungeons", { exp: 303, expAfterLimit: 243, isQuest: true, limit: 4, asFiller: true, time: 150, bams: 4 }],
+    ["kill30Quest", { exp: 270, expAfterLimit: 216, isQuest: true, limit: Infinity, asFiller: true, time: 300, bams: 0 }],
+    ["gather30Quest", { exp: 180, expAfterLimit: 144, isQuest: true, limit: Infinity, asFiller: true, time: 180, bams: 0 }],
     ["carrot", { exp: 60, expAfterLimit: 48, isQuest: true, limit: 1, asFiller: false, time: 150, bams: 0 }],
     ["bam", { exp: 10, expAfterLimit: 10, isQuest: false, limit: Infinity, asFiller: true, time: 8, bams: 0 }],
     ["levelUp65_1", { exp: 1, expAfterLimit: 1, isQuest: false, limit: 1, asFiller: false, time: Infinity, bams: 0 }]

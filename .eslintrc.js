2f0d73fde43a5fee91be33f4bb5527c733244785
Aurelius <aurelius.dev@gmail.com>
2020-10-24 19:23:58 +0200

    ecma version to 2020

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     extends: "eslint:recommended",
     parserOptions: {
-        ecmaVersion: 2017,
+        ecmaVersion: 2020,
         sourceType: "script",
         ecmaFeatures: {
             impliedStrict: true
